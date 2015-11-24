09cb3211259743ab8590ab8c16cdeacd87bc15eb
Iwan in 't Groen <iwanintgroen@gmail.com>
2015-11-25 00:41:15 +0100

    Add coverage to 'normal' requirements.txt

--- setup.py
+++ setup.py
@@ -14,7 +14,8 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
-    'ntplib'
+    'ntplib',
+    'coverage'
 ]
 
 test_requirements = [
