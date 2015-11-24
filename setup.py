a4730e0c75cab4041e48f9eb96aff28492005c9e
Iwan in 't Groen <iwanintgroen@gmail.com>
2015-11-24 21:52:55 +0100

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
-    # TODO: put package requirements here
+    'ntplib'
 ]
 
 test_requirements = [
-    # TODO: put package test requirements here
+    'ntplib'
 ]
 
 setup(
