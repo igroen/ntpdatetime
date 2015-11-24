f95c77cac49b0931de1fa27e4f7bed71571d9762
Iwan in 't Groen <iwanintgroen@gmail.com>
2015-11-24 21:05:09 +0100

    Import ntpdatetime after path is set

--- ntpdatetime/ntpdatetime.py
+++ ntpdatetime/ntpdatetime.py
@@ -1,1 +1,36 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
+import ntplib
+
+
+class NTPDateTime(datetime):
+    _poolservers = (
+        'nl.pool.ntp.org',
+        'europe.pool.ntp.org',
+        'pool.ntp.org',
+    )
+
+    @classmethod
+    def ntp_now(cls):
+        """Returns a datatime object based on the time
+           received from an NTP server"""
+        for poolserver in cls._poolservers:
+            try:
+                response = ntplib.NTPClient().request(
+                    poolserver,
+                    version=3,
+                    timeout=2
+                )
+                return cls.fromtimestamp(response.tx_time)
+            except:
+                continue
+
+        # No poolservers supplied or no resposne from poolservers
+        # Just return the system time for now
+        return cls.now()
+
+
+ntp_now = NTPDateTime.ntp_now
+
+# print(ntp_now().strftime('%d-%m-%Y %H:%M:%S'))
