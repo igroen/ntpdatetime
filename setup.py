120e356403fece238f5d628fd7ce145b504390b6
Iwan in 't Groen <iwanintgroen@gmail.com>
2015-12-01 20:41:46 +0100

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='ntpdatetime',
-    version='0.1.3',
+    version='0.1.4',
     description="Extend datetime module so it can return the time fetched from a NTP server",
     long_description=readme + '\n\n' + history,
     author="Iwan in 't Groen",
