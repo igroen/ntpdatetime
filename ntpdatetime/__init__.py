b5f10bf95f54951062f9b98557bde8bff726ee8b
Iwan in 't Groen <iwanintgroen@gmail.com>
2015-12-02 00:06:01 +0100

    Bump version: 0.1.4 → 0.1.5

--- ntpdatetime/__init__.py
+++ ntpdatetime/__init__.py
@@ -5,4 +5,4 @@ from .ntpdatetime import *  # noqa
 
 __author__ = 'Iwan in \'t Groen'
 __email__ = 'iwanintgroen@gmail.com'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='ntpdatetime',
-    version='0.1.4',
+    version='0.1.5',
     description="Extend datetime module so it can return the time "
                 "fetched from a NTP poolserver",
     long_description=readme + '\n\n' + history,
