6e64cd293e294d4ff0b4009530d43776521eda5c
Iwan in 't Groen <iwanintgroen@gmail.com>
2015-12-01 21:22:25 +0100

    Test flake8 on travis-ci

--- ntpdatetime/config.py
+++ ntpdatetime/config.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-'''Tuple of poolservers'''
+"""Tuple of poolservers
+"""
 
-poolservers = (
+poolservers= (
     'nl.pool.ntp.org',
     'europe.pool.ntp.org',
     'pool.ntp.org',
